85aa3704d73694dd137ee52c75111447028ba876
0xn4de <109149873+0xn4de@users.noreply.github.com>
2024-02-21 21:57:02 +0200

    Remove unneeded vars/functions

--- src/GameReentrancy.sol
+++ src/GameReentrancy.sol
@@ -2,13 +2,10 @@ pragma solidity ^0.8.0;
 
 interface IGame {
     function makeBid() external payable;
-    function bidAddress() external view returns (address);
-    function newBidEtherMin() external view returns (uint256);
 }
 contract GameReentrancy {
     IGame game = IGame(0x52d69c67536f55EfEfe02941868e5e762538dBD6);
     address private owner;
-    bool private flipped;
     uint256 reps;
     constructor() payable {
         owner = msg.sender;
